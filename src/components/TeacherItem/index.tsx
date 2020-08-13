7f6c2f0c28b020aad8dac47b70d4effa2324753c
AndreVTavares <andretavares3@gmail.com>
2020-08-13 10:51:17 -0300

    Creating New Whatsapp Connection function

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 
 import whatsIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
 export interface Teacher {
@@ -19,6 +21,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id 
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
@@ -36,7 +44,12 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/hora:
           <strong>R${teacher.cost}</strong>
         </p>
-        <a href={`https://wa.me/55${teacher.whatsapp}`} target="_blank">
+        <a
+          onClick={createNewConnection}
+          href={`https://wa.me/55${teacher.whatsapp}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={whatsIcon} alt="whatsapp" />
           Entrar em contato
         </a>
